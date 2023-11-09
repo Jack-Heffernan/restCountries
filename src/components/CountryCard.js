083635e13cbc6c21ac3229e0c206def967c07ef3
Jack <jackmh01@gmail.com>
2023-11-09 06:03:57 +0000

    slight styling

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -4,11 +4,12 @@ const CountryCard = (props) => {
     return (
         <div className="card w-96 glass p-4">
             <figure><img classname="" src={props.flag} alt="img"/></figure>
-                <div className="card-body">
+                <div className="card-body items-center text-center">
                     <h2 className="card-title"><Link to={`/country/${props.name}`}>{props.name}</Link></h2>
                     <p>{props.region}</p>
                 <div className="card-actions justify-end">
-            <button className="btn btn-primary" src={`/country/${props.name}`}>Learn now!</button>
+            <a href={`/country/${props.name}`}>
+            <button className="btn btn-secondary" type="button" >View</button> </a>
      </div>
   </div>
 </div>
